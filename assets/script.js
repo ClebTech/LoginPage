f39e36ab701e4083be886b7fa921ac08db5e1841
Clebson Santos <keupsantos@gmail.com>
2025-08-21 11:14:22 -0300

    fixing js bug

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-const eye = document.querySelectorAll(".bx.bx-eye");
-const password = document.querySelectorAll(".password");
+const eyes = document.querySelectorAll(".bx.bx-eye");
+const passwords = document.querySelectorAll(".password");
 
-for (let i = 0; i < 2; i++) { //i = passwords inputs
-    eye[i].addEventListener("click", function () {
-        password[i].type = password[i].type === "password" ? "text" : "password";
-        eye[i].classList.toggle("bx-eye");
-        eye[i].classList.toggle("bx-eye-slash");
+eyes.forEach((eye, i) => {
+    eye.addEventListener("click", () => {
+        passwords[i].type = passwords[i].type === "password" ? "text" : "password";
+        eye.classList.toggle("bx-eye");
+        eye.classList.toggle("bx-eye-slash");
     });
-}
+});
