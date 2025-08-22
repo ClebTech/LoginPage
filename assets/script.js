613d1ac3a098599a25019aae5d1bb2edc957781c
Clebson Santos <keupsantos@gmail.com>
2025-08-22 12:11:21 -0300

    pequenos ajustes

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,11 @@ const passwords = document.querySelectorAll(".password");
 eyes.forEach((eye, i) => {
     eye.addEventListener("click", () => {
         passwords[i].type = passwords[i].type === "password" ? "text" : "password";
+        if(eye.title === "Show password"){
+            eye.title = "Hide password";
+        }else{ 
+            eye.title = "Show password";
+        }
         eye.classList.toggle("bx-eye");
         eye.classList.toggle("bx-eye-slash");
     });
